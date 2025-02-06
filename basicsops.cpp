0f1d9dc2dbc50ac05b91a45bf81ec0308dd6b5aa
Simran Vinodkumar Ahirwar <70149684+simran0809@users.noreply.github.com>
2025-02-06 13:33:24 +0530

    Add files via upload

---
+++ basicsops.cpp
@@ -0,0 +1,30 @@
+// loops is for loops and while loops 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+for(int i=1 ; i<=10 ; i++){
+    cout<<"Hello word"<<endl;
+}
+
+int arr[5];
+
+arr[0]=1;
+arr[1] = 2;
+arr[2]= 13;
+arr[3] = 18;
+arr[4] = 20;
+
+arr[2]=75;
+
+cout<<arr[2];
+
+
+return 0;
+
+
+ // array is the linear datastructure in which we can stored similiar type of data
+
+}
+
